45f296674727437bb8cadf7fc900270de013c466
Ziwen Li <lee23michael@gmail.com>
2019-08-01 22:52:37 -0700

    Using variables

---
+++ src/NonPrimitive.java
@@ -0,0 +1,12 @@
+public class NonPrimitive
+{
+    public static void main(String[] args)
+    {
+        // non-primitive type variable
+
+        String text = "Hello";
+        System.out.println(text);
+
+
+    }
+}
