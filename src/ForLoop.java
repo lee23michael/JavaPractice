dd227470473cca7303f98e0257954e895ce6e0ec
Ziwen Li <lee23michael@gmail.com>
2019-08-01 23:55:57 -0700

    for loop

---
+++ src/ForLoop.java
@@ -0,0 +1,14 @@
+public class ForLoop
+{
+    public static void main(String[] args)
+    {
+        //for loop
+        for(int i = 0 ; i < 15 ; i++) //(initialization; condition, incrementation )
+        {
+            System.out.print(i);
+            System.out.printf("%d\t",i); // %d = integer, \t = add a tab
+        }
+
+        // format specifiers http://alvinalexander.com/programming/printf-format-cheat-sheet
+    }
+}
