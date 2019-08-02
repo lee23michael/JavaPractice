e57168dcb39ea0aca2069a403607c0864bb24244
Ziwen Li <lee23michael@gmail.com>
2019-08-01 23:00:49 -0700

    While loop

---
+++ src/WhileLoops.java
@@ -0,0 +1,16 @@
+public class WhileLoops
+{
+    public static void main(String[] args)
+    {
+        int value = 10;
+        boolean loop = value < 20;
+        System.out.println(loop);
+
+        int num = 0;
+        while(num < 10)
+        {
+            System.out.println("Hello " + num);
+            num = num + 1 ;
+        }
+    }
+}
